940af746ce2763c55df989734d5cf85d6c276526
Arzivre <sonyfauzi@outlook.co.id>
2021-12-09 09:48:31 +0700

    subscribetion update

--- pages/api/stripe-hooks.js
+++ pages/api/stripe-hooks.js
@@ -22,12 +22,21 @@ const handler = async (req, res) => {
   const supabase = getServiceSupabase()
 
   switch (event.type) {
-    case 'customer.subscription.created':
+    case 'customer.subscription.updated':
       await supabase
         .from('profile')
         .update({
           is_subscribed: true,
           interval: event.data.object.items.data[0].plan.interval,
+        })
+        .eq('stripe_customer', event.data.object.customer)
+      break
+    case 'customer.subscription.deleted':
+      await supabase
+        .from('profile')
+        .update({
+          is_subscribed: false,
+          interval: null,
         })
         .eq('stripe_customer', event.data.object.customer)
       break
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,17 +1,30 @@
+import { useRouter } from 'next/router'
+
 import { supabase } from '../utils/supabase'
 import { useUser } from '../context/user'
 
 const Dashboard = () => {
   const { user, isLoading } = useUser()
+  const router = useRouter()
+
+  const loadPortal = async () => {
+    await fetch('/api/portal')
+      .then((response) => response.json())
+      .then((data) => router.push(data.url))
+  }
+
   return (
     <div className='w-full max-w-3xl mx-auto py-16 px-8'>
       <h1 className='text-3xl mb-6'>Dashboard</h1>
       {!isLoading && (
-        <p className='mb-6'>
-          {user?.is_subscribed
-            ? `Subscribed: ${user.interval}`
-            : 'Not subscribed'}
-        </p>
+        <>
+          <p className='mb-6'>
+            {user?.is_subscribed
+              ? `Subscribed: ${user.interval}`
+              : 'Not subscribed'}
+          </p>
+          <button onClick={loadPortal}>Manage subscription</button>
+        </>
       )}
     </div>
   )
